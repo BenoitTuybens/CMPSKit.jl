e692062bce1de226109134ab176a2dc5dcca87b3
BenoitTuybens <benoit.tuybens@ugent.be>
2021-06-17 23:20:21 +0200

    add diagonal form, not working

---
+++ bosons.jl
@@ -0,0 +1,193 @@
+#unique!(push!(LOAD_PATH, "~/Documents/UGent/PhD/Code_cMPS/julia1.0/CMPSKit.jl/src/"))
+unique!(push!(LOAD_PATH, joinpath(pwd(), "src")))
+
+
+using Revise
+using CMPSKit
+using KrylovKit
+using OptimKit
+using LinearAlgebra
+using JLD2
+using TensorOperations
+using Plots
+
+D = 4
+k = 1.
+μ = 1.75
+c = 1.5
+g = 1.
+Λ = 1000.
+gradienttest = false
+tensorproduct = false
+optimisation = true
+lagrangianmultiplier = false
+leftgauge = true
+Zueco = false
+diagonal = true
+
+if tensorproduct
+    if leftgauge
+        #Tensors product ansatz
+        Id = 1*Matrix(I,D,D)
+        KL = Constant(randn(D^2,D^2))
+        KL = 0.5*(KL-KL')
+        R1 = Constant(kron(Id,randn(D,D)))
+        R2 = Constant(kron(randn(D,D),Id))
+        RLs = (R1,R2)
+        QL = KL
+        for R in RLs
+            mul!(QL, R', R, -1/2, 1)
+        end
+        #Put them in cMPS form
+        Ψ = InfiniteCMPS(QL, (R1,R2); gauge = :left)
+    else
+        #Tensors product ansatz
+        Id = 1*Matrix(I,D,D)
+        Q = Constant(randn(D^2,D^2))
+        R1 = Constant(kron(Id,randn(D,D)))
+        R2 = Constant(kron(randn(D,D),Id))
+        #Put them in cMPS form
+        Ψ = InfiniteCMPS(Q, (R1,R2))
+    end
+elseif diagonal
+    #Tensors product ansatz
+    V = Constant(randn(D,D))
+    S1 = Constant(diagm(randn(D)))
+    S2 = Constant(diagm(randn(D)))
+    KL = Constant(randn(D,D))
+    KL = 0.5*(KL-KL')
+    R1 = V*S1*inv(V)
+    R2 = V*S2*inv(V)
+    Ss = (S1,S2)
+    RLs = (R1,R2)
+    QL = KL
+    for R in RLs
+        mul!(QL, R', R, -1/2, 1)
+    end
+    #Put them in cMPS form
+    Ψ = InfiniteCMPS(QL, (R1,R2); gauge = :left)
+else
+    #Random Tensors
+    Q = Constant(randn(D^2,D^2))
+    R1 = Constant(randn(D^2,D^2))
+    R2 = Constant(randn(D^2,D^2))
+    #Put them in cMPS form
+    Ψ = InfiniteCMPS(Q, (R1,R2))
+end
+
+function firstordercorrection(dQ,gradQ,dR1,gradR1,dR2,gradR2)
+    #left gauge: dQ = -R1'*dR1 - R2'*dR2
+    return 2*real(dot(dQ,gradQ) + dot(dR1,gradR1) + dot(dR2,gradR2));
+end
+
+
+alg1 = LBFGS(; verbosity = 2, maxiter = 1000000, gradtol = 1e-4);
+alg2 = ConjugateGradient(; verbosity = 2, maxiter = 1000000, gradtol = 1e-4);
+if leftgauge && tensorproduct
+    linalg = GMRES(krylovdim = 80; tol = 1e-5)
+else
+    linalg = GMRES(krylovdim = 50; tol = 1e-5)
+end
+#Lagrangian multiplier
+
+if lagrangianmultiplier
+    h = k * (∂ψ[1]'*∂ψ[1] + ∂ψ[2]'*∂ψ[2]) - μ * (ψ[1]'*ψ[1] + ψ[2]'*ψ[2]) + c * ((ψ[1]')^2*ψ[1]^2 + (ψ[2]')^2*ψ[2]^2) + Λ * ((ψ[1]*ψ[2] - ψ[2]*ψ[1])' * (ψ[1]*ψ[2] - ψ[2]*ψ[1]))
+    H = ∫(h, (-Inf,+Inf))
+else
+    if Zueco
+        h = k * (∂ψ[1]'*∂ψ[1] + ∂ψ[2]'*∂ψ[2]) - μ * (ψ[1]'*ψ[1] + ψ[2]'*ψ[2]) + c * ((ψ[1]')^2*ψ[1]^2 + (ψ[2]')^2*ψ[2]^2) + g * (ψ[1]'*ψ[2]'*ψ[1]*ψ[2])
+        H = ∫(h, (-Inf,+Inf))
+    else
+        h = k * (∂ψ[1]'*∂ψ[1] + ∂ψ[2]'*∂ψ[2]) - μ * (ψ[1]'*ψ[1] + ψ[2]'*ψ[2]) + c * ((ψ[1]')^2*ψ[1]^2 + (ψ[2]')^2*ψ[2]^2)
+        H = ∫(h, (-Inf,+Inf))
+    end
+end
+
+#h = k * (∂ψ[1]'*∂ψ[1] + ∂ψ[2]'*∂ψ[2]) - μ * (ψ[1]'*ψ[1] + ψ[2]'*ψ[2]) + g * ((ψ[1]')^2*ψ[1]^2 + (ψ[2]')^2*ψ[2]^2) + Λ * ((ψ[1]*ψ[2] - ψ[2]*ψ[1])' * (ψ[1]*ψ[2] - ψ[2]*ψ[1]))
+#H = ∫(k * (∂ψ[1]'*∂ψ[1] + ∂ψ[2]'*∂ψ[2]) - μ * (ψ[1]'*ψ[1] + ψ[2]'*ψ[2]) + g * ((ψ[1]')^2*ψ[1]^2 + (ψ[2]')^2*ψ[2]^2) + Λ * ((ψ[1]*ψ[2] - ψ[2]*ψ[1])' * (ψ[1]*ψ[2] - ψ[2]*ψ[1])), (-Inf,+Inf))
+#h = k * (∂ψ[1]'*∂ψ[1] + ∂ψ[2]'*∂ψ[2]) - μ * (ψ[1]'*ψ[1] + ψ[2]'*ψ[2]) + g * ((ψ[1]')^2*ψ[1]^2 + (ψ[2]')^2*ψ[2]^2)
+#H = ∫(k * (∂ψ[1]'*∂ψ[1] + ∂ψ[2]'*∂ψ[2]) - μ * (ψ[1]'*ψ[1] + ψ[2]'*ψ[2]) + g * ((ψ[1]')^2*ψ[1]^2 + (ψ[2]')^2*ψ[2]^2), (-Inf,+Inf))
+
+if gradienttest
+    ρL, ρR, infoL, infoR = environments!(Ψ;linalg = GMRES(; tol = 1e-6))
+
+    HL, E, e, hL, infoL = leftenv(H, (Ψ,ρL,ρR);linalg = GMRES(; tol = 1e-6))
+    HR, E, e, hR, infoR = rightenv(H, (Ψ,ρL,ρR);linalg = GMRES(; tol = 1e-6))
+
+    gradQ, gradRs = gradient(H, (Ψ, ρL, ρR))
+    dQ = Constant(1e-4*randn(D^2,D^2))
+    dR1 = Constant(kron(Id,1e-4*(randn(D,D))))
+    dR2 = Constant(kron(1e-4*(randn(D,D)),Id))
+    #left gauge dK = -1im*(dR1'*R1 - R1'*dR1 + dR2'*R2 - R2'*dR2)/2
+    Ψ2 = InfiniteCMPS(Q+dQ,(R1+dR1,R2+dR2))
+    foc1 = firstordercorrection(dQ,gradQ,dR1,gradRs[1],dR2,gradRs[2])
+    dQ/=10
+    dR1/=10
+    dR2/=10
+    foc2 = firstordercorrection(dQ,gradQ,dR1,gradRs[1],dR2,gradRs[2])
+    Ψ3 = InfiniteCMPS(Q+dQ,(R1+dR1,R2+dR2))
+    dQ/=10
+    dR1/=10
+    dR2/=10
+    foc3 = firstordercorrection(dQ,gradQ,dR1,gradRs[1],dR2,gradRs[2])
+    Ψ4 = InfiniteCMPS(Q+dQ,(R1+dR1,R2+dR2))
+
+    @show E1 = expval(h,Ψ)
+    E2 = expval(h,Ψ2)
+    E3 = expval(h,Ψ3)
+    E4 = expval(h,Ψ4)
+
+    @show E2[]-E1[]-foc1
+    @show E3[]-E1[]-foc2
+    @show E4[]-E1[]-foc3
+end
+
+
+if optimisation
+    if tensorproduct
+        if leftgauge
+            Ψ, ρR, E, e, normgrad, numfg, history = groundstate4(H, Ψ; optalg = alg1, linalg = linalg)
+            #αs,fs, dfs1, dfs2 = groundstate4(H, Ψ; optalg = alg1, linalg = GMRES(; tol = 1e-5))
+            #αs = (αs[1:end-1] + αs[2:end])/2
+            #push!(αs,0.1)
+            #display(plot(αs,[dfs1,dfs2]))
+            #gui()
+        else
+            Ψ, ρL, ρR, E, e, normgrad, numfg, history = groundstate3(H, Ψ; optalg = alg1, linalg = linalg)
+        end
+        #αs,fs, dfs1, dfs2 = groundstate3(H, Ψ; optalg = alg1, linalg = GMRES(; tol = 1e-5))
+        #αs = (αs[1:end-1] + αs[2:end])/2
+        #push!(αs,0.1)
+    elseif diagonal
+        #Ψ, ρR, E, e, normgrad, numfg, history = groundstate6(H, Ψ, V, Ss; optalg = alg1, linalg = linalg)
+        αs,fs, dfs1, dfs2 = groundstate6(H, Ψ, V, Ss; optalg = alg1, linalg = GMRES(; tol = 1e-5))
+        αs = (αs[1:end-1] + αs[2:end])/2
+        push!(αs,0.1)
+        display(plot(αs,[dfs1,dfs2]))
+        gui()
+    else
+        Ψ, ρR, E, e, normgrad, numfg, history = groundstate(H, Ψ; optalg = alg1, linalg = linalg)
+        #αs,fs, dfs1, dfs2 = groundstate(H, Ψ; optalg = alg1, linalg = GMRES(; tol = 1e-5))
+        #αs = (αs[1:end-1] + αs[2:end])/2
+        #push!(αs,0.1)
+    end
+
+    @show Q = Ψ.Q
+    @show R1 = Ψ.Rs[1]
+    @show R2 = Ψ.Rs[2]
+    @show R1[]*R2[] - R2[]*R1[]
+    @show expval(ψ[1]*ψ[2] - ψ[2]*ψ[1],Ψ)[]
+
+    # n̂₁ = ψ[1]'*ψ[1]
+    # n̂₂ = ψ[2]'*ψ[2]
+    # N̂₂ = ∫(n̂₂, (-Inf,+Inf));
+    # N̂₁ = ∫(n̂₁, (-Inf,+Inf));
+    # ρL = one(ρR)
+    # @show N₁ = expval(n̂₁, Ψ, ρL, ρR)
+    # @show N₂ = expval(n̂₂, Ψ, ρL, ρR)
+    # @show Δρ² = abs(2*tr(leftenv(N̂₂, (Ψ,ρL,ρR))[1]*R1*ρR*R1')[] - expval(n̂₁, Ψ, ρL, ρR)[]*expval(n̂₂, Ψ, ρL, ρR)[])
+    #DR1 = differentiate(R1) + Q * R1 - R1 * Q
+    #DR2 = differentiate(R2) + Q * R2 - R2 * Q
+    #R1² = R1 * R1
+    #R2² = R2 * R2
+end
--- src/infinitecmps/groundstate.jl
+++ src/infinitecmps/groundstate.jl
@@ -477,7 +477,7 @@ function groundstate4(H::LocalHamiltonian, Ψ₀::UniformCMPS;
 
     x, E, normgrad, numfg, history =
         optimize(fg, x, optalg; retract = retract,
-                                precondition = precondition,
+    #                            precondition = precondition,
     #                            finalize! = _finalize!,
                                 inner = inner, transport! = transport!,
                                 scale! = scale!, add! = add!,
@@ -485,4 +485,295 @@ function groundstate4(H::LocalHamiltonian, Ψ₀::UniformCMPS;
     (ΨL, ρR, HL, E, e, hL) = x
     return ΨL, ρR, E, e, normgrad, numfg, history
     #return optimtest(fg, x; retract = retract, inner = inner)
+end
+
+function groundstate5(H::LocalHamiltonian, Ψ₀::UniformCMPS;
+                        optalg = ConjugateGradient(; verbosity = 2, gradtol = 1e-7),
+                        eigalg = defaulteigalg(Ψ₀),
+                        linalg = defaultlinalg(Ψ₀),
+                        finalize! = OptimKit._finalize!,
+                        kwargs...)
+
+    δ = 1
+    function retract(x, d, α)
+        Ψ, = x
+        Q = Ψ.Q
+        Rs = Ψ.Rs
+
+        dQ, dRs = d
+
+        Rs = Rs .+ α .* dRs
+        Q = Q + α * dQ
+
+        Ψ = InfiniteCMPS(Q, Rs)
+        ρR, λ, infoR = rightenv(Ψ; eigalg = eigalg, linalg = linalg, kwargs...)
+        #@show λ
+        #ρL, λ, infoR = leftenv(Ψ; eigalg = eigalg, linalg = linalg, kwargs...)
+        #@show λ
+        ρL, ρR, infoL, infoR = environments!(Ψ; eigalg = eigalg, linalg = linalg, kwargs...)
+        #rmul!(ρR, 1/tr(ρR[]))
+        #rmul!(ρL, 1/tr(ρL[]))
+        HL, E, e, hL, infoL = leftenv(H, (Ψ,ρL,ρR); eigalg = eigalg, linalg = linalg, kwargs...)
+        HR, E, e, hR, infoR = rightenv(H, (Ψ,ρL,ρR); eigalg = eigalg, linalg = linalg, kwargs...)
+
+        dQ = dQ - (λ/α)*one(Q)
+
+        if infoR.converged == 0 || infoL.converged == 0
+            @warn "step $α : not converged, energy = $e"
+            @show infoR
+            @show infoL
+        end
+
+        return (Ψ, ρL, ρR, HL, HR, E, e, hL, hR), (dQ,dRs)
+    end
+
+    transport!(v, x, d, α, xnew) = v # simplest possible transport
+
+    function inner(x, d1, d2)
+        gradQ1, gradRs1 = d1
+        gradQ2, gradRs2 = d2
+        s = gradQ1 === gradQ2 ? 2*norm(gradQ1)^2 : 2*real(dot(gradQ1, gradQ2))
+        for (gradR1,gradR2) in zip(gradRs1, gradRs2)
+            if gradR1 === gradR2
+                s += 2*norm(gradR1)^2
+            else
+                s += 2*real(dot(gradR1, gradR2))
+            end
+        end
+        return s
+    end
+
+    function precondition(x, d)
+        Ψ, ρL, ρR, = x
+        dQ, dRs = d
+        #dQ = posreginv(ρL[0], δ) * dQ * posreginv(ρR[0], δ)
+        dRs = Ref(posreginv(ρL[0], δ)) .* dRs .* Ref(posreginv(ρR[0], δ))
+        return (dQ, dRs)
+    end
+
+    function fg(x)
+        (Ψ, ρL, ρR, HL, HR, E, e, hL, hR) = x
+
+        gradQ, gradRs = gradient(H, (Ψ, ρL, ρR), HL, HR; kwargs...)
+
+        Q = Ψ.Q
+        D = Int(sqrt(size(Q[])[1]))
+        Rs = Ψ.Rs
+
+        Id = 1*Matrix(I,D,D)
+        gradR1 = gradRs[1][]
+        gradR2 = gradRs[2][]
+        gradR1 = reshape(gradR1,D,D,D,D)
+        gradR2 = reshape(gradR2,D,D,D,D)
+        @tensor gradR1[a,b] := gradR1[a,c,b,c]
+        @tensor gradR2[a,b] := gradR2[c,a,c,b]
+        gradR1 = Constant(kron(Id,gradR1/D))
+        gradR2 = Constant(kron(gradR2/D,Id))
+        gradRs = (gradR1,gradR2)
+
+        return E, (gradQ, gradRs)
+    end
+    function scale!(d, α)
+        dQ, dRs = d
+        rmul!(dQ, α)
+        for dR in dRs
+            rmul!(dR, α)
+        end
+        return d
+    end
+    function add!(d1, d2, α)
+        dQ1, dR1s = d1
+        dQ2, dR2s = d2
+        axpy!(α, dQ2, dQ1)
+        for (dR1, dR2) in zip(dR1s, dR2s)
+            axpy!(α, dR2, dR1)
+        end
+        return d1
+    end
+    function _finalize!(x, E, d, numiter)
+        normgrad2 = real(inner(x, d, d))
+        @show normgrad2
+        δ = max(1e-12, 1e-3*normgrad2)
+        return finalize!(x, E, d, numiter)
+    end
+    ρL, ρR, infoL, infoR = environments!(Ψ₀; eigalg = eigalg, linalg = linalg, kwargs...)
+    #rmul!(ρR, 1/tr(ρR[]))
+    #rmul!(ρL, 1/tr(ρL[]))
+    HL, E, e, hL, infoL = leftenv(H, (Ψ₀,ρL,ρR); kwargs...)
+    HR, E, e, hR, infoL = rightenv(H, (Ψ₀,ρL,ρR); kwargs...)
+    x = (Ψ₀, ρL, ρR, HL, HR, E, e, hL, hR)
+
+    x, E, normgrad, numfg, history = optimize(fg, x, optalg; retract = retract,
+    #                            precondition = precondition,
+                                finalize! = _finalize!,
+                                inner = inner, transport! = transport!,
+                                scale! = scale!, add! = add!,
+                                isometrictransport = true)
+
+    (Ψ, ρL, ρR, HL, HR, E, e, hL, hR) = x
+    return Ψ, ρL, ρR, E, e, normgrad, numfg, history
+    #return optimtest(fg, x; retract = retract, inner = inner)
+end
+
+function groundstate6(H::LocalHamiltonian, Ψ₀::UniformCMPS,V,Ss;
+                        optalg = ConjugateGradient(; verbosity = 2, gradtol = 1e-7),
+                        eigalg = defaulteigalg(Ψ₀),
+                        linalg = defaultlinalg(Ψ₀),
+                        finalize! = OptimKit._finalize!,
+                        kwargs...)
+
+    δ = 1
+    function retract(x, d, α)
+        ΨL, V, Ss, = x
+        QL = ΨL.Q
+        RLs = ΨL.Rs
+        KL = copy(QL)
+        for R in RLs
+            mul!(KL, R', R, +1/2, 1)
+        end
+
+        dV, dSs = d
+
+        #RLs = Ref(V) .* Ss .* Ref(inv(V))
+        dRs = Ref(dV) .* Ss .* Ref(inv(V)) .+ Ref(V) .* dSs .* Ref(inv(V)) .- Ref(V) .* Ss .* Ref(inv(V)) .* Ref(dV) .* Ref(inv(V))
+        RdR = zero(KL)
+        for (R, dR) in zip(RLs, dRs)
+            mul!(RdR, R', dR, true, true)
+        end
+
+        V = Constant(exp(α * dV[]) * inv(V)[]) * V
+        Ss = Ss .+ α .* dSs
+
+        KL = KL - (α/2) * (RdR - RdR')
+        QL = KL
+        for R in RLs
+            mul!(QL, R', R, -1/2, 1)
+        end
+        Rls = Ref(V) .* Ss .* Ref(inv(V))
+
+        ΨL = InfiniteCMPS(QL, RLs; gauge = :left)
+        ρR, λ, infoR = rightenv(ΨL; eigalg = eigalg, linalg = linalg, kwargs...)
+        rmul!(ρR, 1/tr(ρR[]))
+        ρL = one(ρR)
+        HL, E, e, hL, infoL =
+            leftenv(H, (ΨL,ρL,ρR); eigalg = eigalg, linalg = linalg, kwargs...)
+
+        if infoR.converged == 0 || infoL.converged == 0
+            @warn "step $α : not converged, energy = $e"
+            @show infoR
+            @show infoL
+        end
+
+        return (ΨL, V, Ss, ρR, HL, E, e, hL), d
+    end
+
+    transport!(v, x, d, α, xnew) = v # simplest possible transport
+
+    function inner(x, d1, d2)
+        dV1, dSs1 = d1
+        dV2, dSs2 = d2
+        s = dV1 === dV2 ? 2*norm(dV1)^2 : 2*real(dot(dV1, dV2))
+        for (dSs1,dSs2) in zip(dSs1, dSs2)
+            if dSs1 === dSs2
+                s += 2*norm(dSs1)^2
+            else
+                s += 2*real(dot(dSs1, dSs2))
+            end
+        end
+        return s
+    end
+
+    function precondition(x, d)
+        ΨL, V, Ss, ρR, = x
+        dK, dRs = d
+
+        Q = ΨL.Q
+        Rs = ΨL.Rs
+        D = Int(sqrt(size(Q[])[1]))
+
+        Id = 1*Matrix(I,D,D)
+        ρR1 = reshape(ρR[0],D,D,D,D)
+        ρR2 = reshape(ρR[0],D,D,D,D)
+        @tensor ρR1[a,b] := ρR1[a,c,b,c]
+        @tensor ρR2[a,b] := ρR2[c,a,c,b]
+
+        dR1 = dRs[1][]
+        dR2 = dRs[2][]
+        dR1 = reshape(dR1,D,D,D,D)
+        dR2 = reshape(dR2,D,D,D,D)
+        @tensor dR1[a,b] := dR1[a,c,b,c]
+        @tensor dR2[a,b] := dR2[c,a,c,b]
+        dR1 = Constant(kron(Id,dR1*posreginv(ρR1, δ)/D))
+        dR2 = Constant(kron(dR2*posreginv(ρR2, δ)/D,Id))
+        dRs = (dR1,dR2)
+
+        return (dK,dRs)
+    end
+
+    function fg(x)
+        (ΨL, V, Ss, ρR, HL, E, e, hL) = x
+
+        gradQ, gradRs = gradient(H, (ΨL, one(ρR), ρR), HL, zero(HL); kwargs...)
+
+        Q = ΨL.Q
+        Rs = ΨL.Rs
+
+        dRs = .-(Rs) .* Ref(gradQ) .+ gradRs
+
+        dSs = Constant.(diagm.((diag.(broadcast(x->x[],(Ref(V') .* dRs .* Ref(inv(V)')))))))
+
+        dV = zero(V)
+        for (R, dR) in zip(Rs,dRs)
+            dV += (dR*R' - R'*dR) 
+        end
+
+        return E, (dV, dSs)
+    end
+
+    function scale!(d, α)
+        dV, dSs = d
+        rmul!(dV, α)
+        for dS in dSs
+            rmul!(dS, α)
+        end
+        return d
+    end
+    function add!(d1, d2, α)
+        dV1, dS1s = d1
+        dV2, dS2s = d2
+        axpy!(α, dV2, dV1)
+        for (dS1, dS2) in zip(dS1s, dS2s)
+            axpy!(α, dS2, dS1)
+        end
+        return d1
+    end
+
+    function _finalize!(x, E, d, numiter)
+        normgrad2 = real(inner(x, d, d))
+        δ = max(1e-12, 1e-3*normgrad2)
+        return finalize!(x, E, d, numiter)
+    end
+
+    ΨL₀ = Ψ₀
+    #ΨL₀, = leftgauge(Ψ₀; kwargs...)
+    ρR, λ, infoR = rightenv(ΨL₀; kwargs...)
+    ρL = one(ρR)
+    rmul!(ρR, 1/tr(ρR[]))
+    HL, E, e, hL, infoL = leftenv(H, (ΨL₀,ρL,ρR); kwargs...)
+    #Rs = Ψ₀.Rs
+    #V = Constant(eigvecs(Rs[1][]))
+    #Ss = Constant.(diagm.(eigvals.(broadcast(x->x[],Rs))))
+
+    x = (ΨL₀, V, Ss, ρR, HL, E, e, hL)
+
+    #x, E, normgrad, numfg, history =
+    #    optimize(fg, x, optalg; retract = retract,
+    #                            precondition = precondition,
+    #                            finalize! = _finalize!,
+    #                            inner = inner, transport! = transport!,
+    #                            scale! = scale!, add! = add!,
+    #                            isometrictransport = true)
+    #(ΨL, V, Ss, ρR, HL, E, e, hL) = x
+    #return ΨL, ρR, E, e, normgrad, numfg, history
+    return optimtest(fg, x; retract = retract, inner = inner)
 end
